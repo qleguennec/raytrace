9146dc8c071ec7710b919c75b1580c196951ab64
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2017-03-02 10:01:15 +0100

    fix file names

--- apply_mat.c
+++ apply_mat.c
@@ -1,48 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   v3_v3_op.c                                         :+:      :+:    :+:   */
+/*   apply_mat.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/02/28 14:05:33 by qle-guen          #+#    #+#             */
-/*   Updated: 2017/02/28 14:36:36 by qle-guen         ###   ########.fr       */
+/*   Created: 2017/03/02 09:57:58 by qle-guen          #+#    #+#             */
+/*   Updated: 2017/03/02 10:00:26 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "obj.h"
-
-t_v3
-	add
-	(t_v3 a
-	, t_v3 b)
-{
-	return (V3(a.x + b.x, a.y + b.y, a.z + b.z));
-}
-
-t_v3
-	sub
-	(t_v3 a
-	, t_v3 b)
-{
-	return (V3(a.x - b.x, a.y - b.y, a.z - b.z));
-}
-
-t_v3
-	mult
-	(t_v3 a
-	, t_v3 b)
-{
-	return (V3(a.x * b.x, a.y * b.y, a.z * b.z));
-}
-
-t_v3
-	scale
-	(float x
-	, t_v3 a)
-{
-	return (V3(x * a.x, x * a.y, x * a.z));
-}
+#include "rt.h"
 
 t_v3
 	apply_mat
